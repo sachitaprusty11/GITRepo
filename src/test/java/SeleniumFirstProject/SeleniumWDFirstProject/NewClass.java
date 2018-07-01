19cce11dc2f47c9bf1abf05147e7093c24f80751
Sachita <sachitaprusty11@gmail.com>
2018-07-01 21:11:13 +0530

    Added new class

---
+++ src/test/java/SeleniumFirstProject/SeleniumWDFirstProject/NewClass.java
@@ -0,0 +1,14 @@
+package SeleniumFirstProject.SeleniumWDFirstProject;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+
+	
+	@Test
+	public void login() throws IOException{
+		System.out.println("New Class");
+	}
+}
